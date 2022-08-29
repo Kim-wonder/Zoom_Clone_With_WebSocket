1455a2e94ee8859da11e446b7f7388b509f6507d
김혜진 <>
2022-08-29 19:59:43 +0900

    [update] 개발환경 구축

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 import express from 'express';
 
+// express가 할 일은 별로 없다.
+// view를 설정해주고 render 해주면 끝. 
+// 나머지는 웹소켓 안에서 실시간으로 일어난다.
+
 const app = express();
 
-console.log("hello!");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get('/', (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => console.log("Listening on http://localhost:3000");
+app.listen(3000,handleListen);
