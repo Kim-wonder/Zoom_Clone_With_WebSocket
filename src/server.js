b6585c1d9c7f1b173af184b639f145e2075cb28b
김혜진 <>
2022-08-29 20:13:17 +0900

    [add] 서버환경구축 설명 추가

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import req from 'express/lib/request';
 
 // express가 할 일은 별로 없다.
 // view를 설정해주고 render 해주면 끝. 
@@ -10,6 +11,7 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get('/', (req, res) => res.render("home"));
+app.get('/*', (req,res) => res.redirext("/*"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 app.listen(3000,handleListen);
