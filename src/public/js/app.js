dbae1018e00d297b290f6fb92352d84d82af2465
김혜진 <>
2022-09-01 01:10:26 +0900

    [update] 닉네임 구분과 메시지 연결 ~1.8

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -26,13 +26,14 @@ socket.addEventListener("close", () => {
 function handleSubmit (event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message:", input.value));
+    socket.send(makeMessage("new_message", input.value));
     input.value = "";
 };
 function handleNickSubmit (event) {
     event.preventDefault();
     const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname:", input.value));
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "";
 }
 
 messageForm.addEventListener('submit', handleSubmit);
